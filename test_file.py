48fade1541bc8b606643dd975aebc1a15d3a7f46
KrehlK <krehlkasayan@gmail.com>
2021-04-02 00:49:26 -0400

    added test_file.py

---
+++ test_file.py
@@ -0,0 +1,13 @@
+""" 
+a test file to see if git is working
+"""
+
+class person:
+    """
+    a person in a network
+    
+    Attributes:
+        name(str): the name
+        connections(set of Person): other ppl
+
+    """
