f79b7a6cbf32546448742625d61d3d36f142cd3e
Abdul Basit Razack <basitrazackak@gmail.com>
2021-04-12 00:03:53 -0400

    doc string

--- test_file.py
+++ test_file.py
@@ -18,13 +18,10 @@ class Customer:
     
     Attributes:
         name(str): the name
-        connections(set of Person): other ppl
-
+        
     """
     
-    
-
-    
+       
 class Bank_account(Bank):
     
     
@@ -39,29 +36,101 @@ class saving(Bank):
     
 def deposit account:
 
+"""
+    This will show each customers account balance, and any other accounts they have
+    
+    Attributes:
+        name(str): the name
+        num: account numbers 
+          
+    """
 
 
 def deposit transactions:
 
+"""
+    This will show each customers transaction history.
+    
+    Attributes:
+        name: account name
+        num: dollar amount
+          
+    """
+
 
 def withdrawal:
-
+    
+"""
+    This will show each customers withdrawl
+    
+    Attributes:
+        num: dollar amount
+          
+    """
 
 def transfer:
+
+"""
+    This will show each customers transfer
+    
+    Attributes:
+        name: account name
+        num: dollar amount
+          
+    """
 
 
 def spending tracker:
 
+"""
+    This will show how much each customer spends
+    
+    Attributes:
+        name: account name
+        num: dollar amount
+        percent: the percentage amount of their spending
+          
+    """
 
 
 def income to saving ratio:
+    
+
 
 
 def alerts:
+    
+    
+"""
+    This will alert customers if they have spend over their limit 
+
+        Attributes:
+        num: dollar amount
+          
+    """
 
 def reward:
+    
+"""
+    This will show each customer how much they have earned from their spending 
+
+        Attributes:
+        num: dollar amount
+        point: amount of points they have earned 
+          
+    """
+
 
 def schedule appointments: 
+    
+    """
+    This allow customer to make appointment to meet with their advior
+
+        Attributes:
+        date: available dates
+        time: availabe times and the duration
+              
+    """
 
 
 
