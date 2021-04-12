89f1bb508849f96f7c41a8df04ec7c1f83da94ab
Abdul Basit Razack <basitrazackak@gmail.com>
2021-04-11 23:43:47 -0400

    added methods

--- test_file.py
+++ test_file.py
@@ -13,5 +13,58 @@ class Bank:
     """
     
 class Customer:
+     """
+    Each customers information with a bank account 
+    
+    Attributes:
+        name(str): the name
+        connections(set of Person): other ppl
+
+    """
     
     
+
+    
+class Bank_account(Bank):
+    
+    
+        
+    
+class Transactions:
+    def __init__(self, name)
+        self.name = name
+        
+        
+class saving(Bank):
+    
+def deposit account:
+
+
+
+def deposit transactions:
+
+
+def withdrawal:
+
+
+def transfer:
+
+
+def spending tracker:
+
+
+
+def income to saving ratio:
+
+
+def alerts:
+
+def reward:
+
+def schedule appointments: 
+
+
+
+
+
+
