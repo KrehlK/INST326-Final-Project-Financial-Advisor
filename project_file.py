4aca609b784a176f129a8b69fd00a712aa68e8f5
KrehlK <krehlkasayan@gmail.com>
2021-04-16 09:43:32 -0400

    deleted a bumnch of shit

--- project_file.py
+++ project_file.py
@@ -22,21 +22,37 @@ class Bank:
                 account = {"name": name, "Account Number": account_number, 
                            "balance": balance}
                 db.append(account)
-    
+
     def transaction(self, action):
         """
         an transaction that removes or adds money
         """
         self.balance = self.balance + action
         
-    def display(self):
-        print()
-                
-    
-                
-                
-                
+    def reciept(self):
+        """
+        Side effects:
+            print(): prints to terminal
+        """
+        print(f"Account Summary 
+              Account Owner: {name} 
+              Account Number: {account_number} 
+              Balance: {balance}")
         
+    def saving(self):
+        # for abdul: is the savings moreso transferring money
+        # to savings or adding money?
+    
+    def credit:
+        """
+        1000
+        *.8
+        _____
+        800> 200$ 
+        return boolean 
+        (yes)
+        customer can apply credit card
+        """
     
 class Customer:
      """
@@ -46,112 +62,9 @@ class Customer:
         name(str): the name
         
     """
+    def __init__():
     
        
-class Bank_account(Bank):
-    
-    
-        
-        
-class saving(Bank):
-    
-def deposit account:
-
-"""
-    This will show each customers account balance, and any other accounts they have
-    
-    Attributes:
-        name(str): the name
-        num: account numbers 
-          
-    """
-
-
-def deposit transactions:
-
-"""
-    This will show each customers transaction history.
-    
-    Attributes:
-        name: account name
-        num: dollar amount
-          
-    """
-
-
-def withdrawal:
-    
-"""
-    This will show each customers withdrawl
-    
-    Attributes:
-        num: dollar amount
-          
-    """
-
-def transfer:
-
-"""
-    This will show each customers transfer
-    
-    Attributes:
-        name: account name
-        num: dollar amount
-          
-    """
-
-
-def spending tracker:
-
-"""
-    This will show how much each customer spends
-    
-    Attributes:
-        name: account name
-        num: dollar amount
-        percent: the percentage amount of their spending
-          
-    """
-
-
-def income to saving ratio:
-    
-
-
-
-def alerts:
-    
-    
-"""
-    This will alert customers if they have spend over their limit 
-
-        Attributes:
-        num: dollar amount
-          
-    """
-
-def reward:
-    
-"""
-    This will show each customer how much they have earned from their spending 
-
-        Attributes:
-        num: dollar amount
-        point: amount of points they have earned 
-          
-    """
-
-
-def schedule appointments: 
-    
-    """
-    This allow customer to make appointment to meet with their advior
-
-        Attributes:
-        date: available dates
-        time: availabe times and the duration
-              
-    """
 
 
 
