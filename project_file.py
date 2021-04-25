3f4998975c4084d9f68383c46d456ed75a366429
Abdul Basit Razack <basitrazackak@gmail.com>
2021-04-25 11:05:54 -0400

    added __init__ git add project_file.py

--- project_file.py
+++ project_file.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+
+        
 class Bank:
     """
     A bank with a person's bank account information
@@ -9,85 +11,51 @@ class Bank:
         connections(set of Person): other ppl
 
     """
-    def __init__(self, filepath):
-        self.pd = read_csv("INST326projectdata.csv",
-                           sep= "\t", index_col = "Account Number")
-        # self.account_number = 
-        # self.first_name = 
-        # self.last_name =  
-        # self.email = 
-        # self.phone = 
-        # self.balance =  
-        # self.credit = 
-        # self.credit_score = 
-    
+    def __init__(self, accountnumber):
         
-
-    def transaction(self, action):
+        # to test collume to be search... ideially it will 
+        search_firstname = accountnumber
+        
+        df = pd.read_csv("INST326projectdata.csv")
+        
+        # Choose entries with first name
+        df_new = df[df['First Name'] == search_firstname]
+        #This should change to account later, have to fix cvs format first.
+        
+        print("init")
+        #print(str(self.data))
+        
+        print(df_new)
+        
+        self.account_number = df_new["Account Number"]
+        self.first_name = df_new["First Name"]
+        self.last_name =  df_new["Last Name"]
+        self.email = df_new["Email"]
+        self.phone = df_new["Phone"]
+        self.balance = df_new["Balance"] 
+        self.credit = df_new["Credit"]        
+        self.credit_score = df_new["Credit Score"] 
+        self.recurring_payments = df_new["Recurring Payments"] 
+        
+        
+        print(self.last_name + " " + self.email )
+    #this is a test
+    
+    
+    def transaction(self):
+        print("transaction")
         """
         an transaction that removes or adds money
-        
+
         Args:
-            self: instance of Bank
-            action: a integer that can be negative or positive
+        self: instance of Bank
+        action: a integer that can be negative or positive
         """
-        self.balance = self.balance + action
-        
-    def reciept(self):
-        """
-        prints out the status of the bank account
-        
-        Args:
-            self: instance of receipt
-        Side effects:
-            print(): prints to terminal
-        """
-        print(f"Account Summary 
-              Account Owner: {name} 
-              Account Number: {account_number} 
-              Balance: {balance}")
-        
-    def saving(self):
-       """ 
-       This Method will be used to keep track of the person’s saving. 
-       In this option, the person will be able to input any other places 
-       they may have their money saved. In this method, the person will also 
-       have the option to input if they withdraw any money
-       
-       """
-    
-    def credit(self, account):
-        """
-            This method uses the amount of money in a 
-            users account and determines if they qualify for a credit card.
+        #self.balance = self.balance + action
 
-            Args:
-                account:(user's account)
 
-            Returns:
-                A TRUE if they qualify or a FALSE if they don't? 
-        """
-        
-    
-class Customer:
-    """
-    Each customers information with a bank account 
-    
-    Attributes:
-        name(str): the name
-        
-    """
-    def __init__(name, bank_account):
-        self.name = name
-        self.wallet = 0.0
-        self.bank_account = bank_account
-    
-    def withdraw():
-        """
-        Withdraws money from the bank account and adds it to the wallet
-        """
 
-    def deposit():
-        """
-        Deposits money into the bank account and removes it from the wallet
-        """
+print("start here")
+test1 = Bank("Michael")
+test1.transaction()
+#this is passing via name for now, change to 
